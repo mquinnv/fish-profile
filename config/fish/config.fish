bff426db3ce8ed058b618d02495cc33948bbffc9
Michael Ventura <michael@aluminum.local>
2015-10-01 12:27:22 -0400

    switched to new omf boostrap

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,13 @@
 # Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+set -g OMF_PATH $HOME/.local/share/omf
 
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+### Configuration required to load oh-my-fish ###
+# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
+# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
+# to create a custom plugin instead.
 
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+# # Load oh-my-fish configuration.
+ source $OMF_PATH/init.fish
 
 # Custom plugins and themes may be added to ~/.oh-my-fish/custom
 # Plugins and themes can be found at https://github.com/oh-my-fish/
