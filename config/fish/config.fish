03e1fffcae3510c9513137a4ca8fcfb22cc6857e
Michael Ventura <michael.ventura@gmail.com>
2016-03-17 13:19:13 -0400

    set user paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -75,10 +75,5 @@ function fuck -d "Correct your previous console command"
 end
 function fish_greeting
 end
-function fish_vi_key_bindings_local
-  fish_vi_key_bindings
-  bind -e -M insert \cc
-  bind -M insert -m insert \cc 'commandline ""'
-end
-set -g fish_key_bindings fish_vi_key_bindings_local
 set -g theme_hostname always
+set -g fish_user_paths {/usr,/usr/local,}/{bin,sbin} ~/bin
